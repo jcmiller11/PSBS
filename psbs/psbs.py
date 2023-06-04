aa3bb978bd20f9273689975814a3b5abbc17f0c2
John C Miller <jcmiller@berkshirephotonics.com>
2023-06-04 09:02:23 -0400

    Refactored Functions into Project Class
    Built the PSBSProject class to contain the various functions as well as the config variable that interact with the user's project

--- psbs/psbs.py
+++ psbs/psbs.py
@@ -7,19 +7,6 @@ from sys import argv
 from os import environ, getenv
 from gistyc import GISTyc
 
-def load_config():
-    global config
-    #FIXME: refactor as psbs project class to remove global
-    try:
-        with open("config.toml", "rb") as config_file:
-            config = tomllib.load(config_file)
-    except IOError as err:
-        print(f"Error: Config file not found\n  {err}")
-        raise SystemExit(1) from err
-
-class GistError(Exception):
-    """Thrown when GitHub refuses to create or update the gist"""
-
 def get_token():
     if "PSBS_GH_TOKEN" in environ:
         return getenv('PSBS_GH_TOKEN')
@@ -32,101 +19,114 @@ def get_token():
         print("ERROR: gh-cli refuses to provide token, aborting upload")
         raise SystemExit(1) from err
 
-def build():
-    # Build the readme.txt
-    print ("Writing file bin/readme.txt")
-    try:
-        with open("bin/readme.txt", "w", encoding='UTF-8') as readme_file:
-            readme_file.write("Play this game by pasting the script in ")
-            readme_file.write(f"{config['engine']}editor.html")
-    except IOError as err:
-        print(f"Error: Unable to write file readme.txt\n  {err}")
-        raise SystemExit(1) from err
-    except KeyError as err:
-        print(f"Error: Unable to find {err} directive in config file")
-        raise SystemExit(1) from err
-
-    # Build the script.txt
-    print ("Building script.txt")
-    layout = {
-        "prelude":"",
-        "objects":"",
-        "legend":"",
-        "sounds":"",
-        "collisionlayers":"",
-        "rules":"",
-        "winconditions":"",
-        "levels":""
-    }
-
-    for key in layout:
+class PSBSProject:
+    def __init__(self, config_filename = "config.toml"):
+        print(config_filename)
         try:
-            for src_filename in config[key]:
-                print(f" Importing file src/{src_filename}")
-                try:
-                    with open(f"src/{src_filename}", "r", encoding='UTF-8') as src_file:
-                        layout[key] += f"{src_file.read()}\n\n"
-                except IOError as err:
-                    print(f"Warning: Unable to read file {src_filename}\n  {err}")
+            with open(config_filename, "rb") as config_file:
+                self.config = tomllib.load(config_file)
+        except IOError as err:
+            print(f"Error: Config file not found\n  {err}")
+            raise SystemExit(1) from err
+
+    class GistError(Exception):
+        """Thrown when GitHub refuses to create or update the gist"""
+
+    def build(self):
+        # Build the readme.txt
+        print ("Writing file bin/readme.txt")
+        try:
+            with open("bin/readme.txt", "w", encoding='UTF-8') as readme_file:
+                readme_file.write("Play this game by pasting the script in ")
+                readme_file.write(f"{self.config['engine']}editor.html")
+        except IOError as err:
+            print(f"Error: Unable to write file readme.txt\n  {err}")
+            raise SystemExit(1) from err
         except KeyError as err:
-            print(f"Warning: Unable to find {err} directive in config file")
+            print(f"Error: Unable to find {err} directive in config file")
+            raise SystemExit(1) from err
 
-    def make_section(name,has_title = True):
-        if has_title:
-            return f"========\n{name.upper()}\n========\n\n{layout[name]}"
-        return layout[name]
+        # Build the script.txt
+        print ("Building script.txt")
+        layout = {
+            "prelude":"",
+            "objects":"",
+            "legend":"",
+            "sounds":"",
+            "collisionlayers":"",
+            "rules":"",
+            "winconditions":"",
+            "levels":""
+        }
 
-    source = make_section("prelude", False)
-    source += make_section("objects")
-    source += make_section("legend")
-    source += make_section("sounds")
-    source += make_section("collisionlayers")
-    source += make_section("rules")
-    source += make_section("winconditions")
-    source += make_section("levels")
+        for key in layout:
+            try:
+                for src_filename in self.config[key]:
+                    print(f" Importing file src/{src_filename}")
+                    try:
+                        with open(f"src/{src_filename}", "r", encoding='UTF-8') as src_file:
+                            layout[key] += f"{src_file.read()}\n\n"
+                    except IOError as err:
+                        print(f"Warning: Unable to read file {src_filename}\n  {err}")
+            except KeyError as err:
+                print(f"Warning: Unable to find {err} directive in config file")
 
-    print ("Writing file bin/script.txt")
-    try:
-        with open("bin/script.txt", "w", encoding='UTF-8') as readme_file:
-            readme_file.write(source)
-    except IOError as err:
-        print(f"Error: Unable to write file script.txt\n  {err}")
-        raise SystemExit(1) from err
+        def make_section(name,has_title = True):
+            if has_title:
+                return f"========\n{name.upper()}\n========\n\n{layout[name]}"
+            return layout[name]
 
-def upload():
-    print("Updating gist")
-    try:
-        gist_id = config['gist_id']
-    except KeyError as err:
-        print(f"Error: Unable to find {err} directive in config file")
-        raise SystemExit(1) from err
+        source = make_section("prelude", False)
+        source += make_section("objects")
+        source += make_section("legend")
+        source += make_section("sounds")
+        source += make_section("collisionlayers")
+        source += make_section("rules")
+        source += make_section("winconditions")
+        source += make_section("levels")
 
-    token = get_token()
-    try:
-        gist_api = GISTyc(auth_token=token)
-        response_update_data = gist_api.update_gist(file_name="bin/readme.txt",gist_id=gist_id)
-        if "message" in response_update_data:
-            raise GistError(response_update_data['message'])
-        response_update_data = gist_api.update_gist(file_name="bin/script.txt",gist_id=gist_id)
-        if "message" in response_update_data:
-            raise GistError(response_update_data['message'])
-    except ConnectionError as err:
-        print("Error: Unable to connect to GitHub, aborting upload")
-        raise SystemExit(1) from err
-    except GistError as err:
-        print(f"Error: Unable to update gist\n  Response: {err}")
-        raise SystemExit(1) from err
+        print ("Writing file bin/script.txt")
+        try:
+            with open("bin/script.txt", "w", encoding='UTF-8') as readme_file:
+                readme_file.write(source)
+        except IOError as err:
+            print(f"Error: Unable to write file script.txt\n  {err}")
+            raise SystemExit(1) from err
 
-def run():
-    print("Opening in browser")
-    try:
-        webbrowser.open(f"{config['engine']}play.html?p={config['gist_id']}", new=2)
-    except KeyError as err:
-        print(f"Error: Unable to find {err} directive in config file")
-        raise SystemExit(1) from err
-    except webbrowser.Error as err:
-        print("Error: Unable to find user preferred browser to launch")
-        raise SystemExit(1) from err
+    def upload(self):
+        print("Updating gist")
+        try:
+            gist_id = self.config['gist_id']
+        except KeyError as err:
+            print(f"Error: Unable to find {err} directive in config file")
+            raise SystemExit(1) from err
+
+        token = get_token()
+        try:
+            gist_api = GISTyc(auth_token=token)
+            response_update_data = gist_api.update_gist(file_name="bin/readme.txt",gist_id=gist_id)
+            if "message" in response_update_data:
+                raise self.GistError(response_update_data['message'])
+            response_update_data = gist_api.update_gist(file_name="bin/script.txt",gist_id=gist_id)
+            if "message" in response_update_data:
+                raise self.GistError(response_update_data['message'])
+        except ConnectionError as err:
+            print("Error: Unable to connect to GitHub, aborting upload")
+            raise SystemExit(1) from err
+        except self.GistError as err:
+            print(f"Error: Unable to update gist\n  Response: {err}")
+            raise SystemExit(1) from err
+
+    def run(self):
+        print("Opening in browser")
+        try:
+            webbrowser.open(f"{self.config['engine']}play.html?p={self.config['gist_id']}", new=2)
+        except KeyError as err:
+            print(f"Error: Unable to find {err} directive in config file")
+            raise SystemExit(1) from err
+        except webbrowser.Error as err:
+            print("Error: Unable to find user preferred browser to launch")
+            raise SystemExit(1) from err
 
 def new():
     print("make a new project")
@@ -149,17 +149,17 @@ def main():
     if len(argv)>1:
         first_arg = argv[1].lower()
         if first_arg == "build":
-            load_config()
-            build()
+            project = PSBSProject()
+            project.build()
         elif first_arg == "upload":
-            load_config()
-            build()
-            upload()
+            project = PSBSProject()
+            project.build()
+            project.upload()
         elif first_arg == "run":
-            load_config()
-            build()
-            upload()
-            run()
+            project = PSBSProject()
+            project.build()
+            project.upload()
+            project.run()
         elif first_arg == "new":
             new()
         elif first_arg == "help":
